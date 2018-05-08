9a5c74a8e44b4f0930627dda98b55eb2e1476b9d
James Young <james.s.young@gmail.com>
2018-05-07 21:11:40 -0700

    update to 0.4.23, use 'localhost' instead of '127.0.0.1', remove gas limit

--- contracts/SpankBank.sol
+++ contracts/SpankBank.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.4.15;
+pragma solidity 0.4.23;
 import {SafeMath} from './SafeMath.sol';
 import {HumanStandardToken} from './HumanStandardToken.sol';
 import {MintableToken} from './MintableToken.sol';
